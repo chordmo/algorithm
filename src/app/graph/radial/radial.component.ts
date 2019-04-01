2c393bdf5412a242a602a1c58bb72f3b29b3992c
chordmo <chordmo@gmail.com>
2019-04-01 16:17:55 +0800

    angle

--- src/app/graph/radial/radial.component.ts
+++ src/app/graph/radial/radial.component.ts
@@ -21,13 +21,12 @@ export class RadialComponent implements OnInit {
 
     const ctx: CanvasRenderingContext2D =
       this.canvasRef.nativeElement.getContext('2d');
-    //  .getContext('2d');
     ctx.beginPath();
     ctx.arc(x, y, 1, 0, 2 * Math.PI);
     ctx.stroke();
 
 
-    for (let a = 0; a <= 360; a += 10) {
+    for (let a = 0; a <= 350; a += 10) {
       console.log(a);
       const ax = x + r * Math.cos(a * Math.PI / 180);
 
@@ -40,6 +39,39 @@ export class RadialComponent implements OnInit {
 
 
 
+
+
+
+
+    console.log(this.girth(100, 90));
+
+
+  }
+  /**
+   * 求一个角度的周长
+   * @param r 圆的半径
+   * @param a 角的大小
+   */
+  girth(r, a) {
+    return 2 * Math.PI * r * (a / 360);
+  }
+
+  /**
+   * 给一个周长来求圆的半径
+   * @param g 圆的周长
+   * @param a 角的大小
+   */
+  radius(g, a) {
+    return g / 2 * Math.PI * (a / 360);
+  }
+
+  /**
+   * 求角度的大小
+   * @param g 圆的周长
+   * @param r 圆的半径
+   */
+  angle(g, r) {
+    return g / 2 * Math.PI * r * 360;
   }
 
 }
